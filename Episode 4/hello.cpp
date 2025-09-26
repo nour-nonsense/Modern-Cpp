fcd1b9cb06aecd1ef0da36c58914a9b225ea59fc
nour-nonsense <nourkarammahmoud@gmail.com>
2025-09-26 22:09:56 +0300

    Add Episode 4: Hello World in C++

---
+++ Episode 4/hello.cpp
@@ -0,0 +1,21 @@
+#include <iostream> // Includes the input/output stream library for console output
+
+int main() {
+    // this is the main entry point of the program
+    // Every C++ program must have a main() function
+
+    // Example of printing "Hello World!" with a newline using \n
+    // std::cout << "Hello World!\n";
+
+    // Example of printing "Hello World!" with multiple newlines using std::endl
+    // std::cout << "Hello World!" << std::endl << std::endl << std::endl;
+
+    // Example of printing "Hello World!" with single newline using std::endl
+    // std::cout << "Hello World!" << std::endl;
+    
+    // Actual line that prints "Hello World!" with a single newline
+    std::cout << "Hello World!" << std::endl;
+
+    // Return EXIT_SUCCESS (usually 0) to indicate the program finished successfully
+    return EXIT_SUCCESS;
+}
